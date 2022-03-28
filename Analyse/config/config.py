9dd41fef930422959f0888c3741c74a478d4812b
JohnNuwan <john.nuwan.moncel@gmail.com>
2022-03-28 11:22:26 +0200

    Update & Analyse & gitignore

---
+++ Analyse/config/config.py
@@ -0,0 +1,14 @@
+
+from rich import print
+from rich.console import Console
+
+cs = Console()
+
+#__________________________________________________
+# Definition Serveur
+host = "localhost"
+port = 8095
+debug = True
+url = f"http://{host}:{port}"
+
+#__________________________________________________
